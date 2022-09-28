91a8c9775b256ebaee7eca2f9cdd6bf22efcd0d1
Brenorml <breno.lucena@gmail.com>
2022-09-28 15:09:35 +0100

    added class people and calculator

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using exemploFundamentos.models;
+
+// Pessoa p = new Pessoa();
+
+// p.Nome = "Breno";
+// p.Idade = 40;
+// p.Apresentar();
+
+// string a = "15";
+// int b = 0;
+
+// int.TryParse(a,  out b);
+
+
+// Console.WriteLine(b);
+// Console.WriteLine("Conversão realizada com sucesso!!!");
+
+// int quantidadeEmEstoque = 10;
+// int quantidadeCompra = 4;
+// bool possivelVenda = quantidadeEmEstoque >= quantidadeCompra;
+
+// Console.WriteLine("Quantidade em Estoque: " + quantidadeEmEstoque);
+// Console.WriteLine("Quantidade compra: " + quantidadeCompra);
+// Console.WriteLine("É possível realizar a venda?: " + possivelVenda);
+
+// if(possivelVenda) {
+//     Console.WriteLine("Venda realizada com sucesso!");
+// } else {
+//     Console.WriteLine("Desculpe. Não temos quantidade desejada em estoque.");
+// }
+
+Calculadora calc = new Calculadora();
+
+calc.somar(2, 3);
+calc.subtrair(2, 3);
+calc.multiplicar(2, 3);
+calc.dividir(18, 3);
---
+++ models/Calculadora.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace exemploFundamentos.models
+{
+    public class Calculadora
+    {
+        public void somar(int x, int y)
+        {
+            Console.WriteLine($"{x} + {y} = {x + y}");
+        }
+        public void subtrair(int x, int y)
+        {
+            Console.WriteLine($"{x} - {y} = {x - y}");
+        }
+        public void multiplicar(int x, int y)
+        {
+            Console.WriteLine($"{x} x {y} = {x * y}");
+        }
+        public void dividir(int x, int y)
+        {
+            Console.WriteLine($"{x} / {y} = {x / y}");
+        }
+    }
+}
