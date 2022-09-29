1759165fde41203b181589ecd141bfdfc9ee969a
Brenorml <breno.lucena@gmail.com>
2022-09-29 14:46:57 +0100

    analyzed how a C# program is organized

--- ExmploFundamentos/Program.cs
+++ ExmploFundamentos/Program.cs
@@ -42,44 +42,44 @@ p.Apresentar();
 // calc.Tangente(30);
 // calc.RaizQuadrada(9);
 
-// string opcao;
-// bool exibirMenu = true;
+//  string opcao;
+//  bool exibirMenu = true;
 
-// while(exibirMenu)
-// {
-//     Console.Clear();
-//     Console.WriteLine("Digite a sua opção:");
-//     Console.WriteLine("1 - Cadastrar cliente");
-//     Console.WriteLine("2 - Buscar cliente");
-//     Console.WriteLine("3 - Apagar cliente");
-//     Console.WriteLine("4 - Encerrar");
+//  while(exibirMenu)
+//  {
+//      Console.Clear();
+//      Console.WriteLine("Digite a sua opção:");
+//      Console.WriteLine("1 - Cadastrar cliente");
+//      Console.WriteLine("2 - Buscar cliente");
+//      Console.WriteLine("3 - Apagar cliente");
+//      Console.WriteLine("4 - Encerrar");
 
-//     opcao = Console.ReadLine();
+//      opcao = Console.ReadLine();
 
-//     switch(opcao)
-//     {
-//         case "1":
-//         Console.WriteLine("Cadastro de cliente");
-//         break;
+//      switch(opcao)
+//      {
+//          case "1":
+//          Console.WriteLine("Cadastro de cliente");
+//          break;
 
-//         case "2":
-//         Console.WriteLine("Busca de cliente");
-//         break;
+//          case "2":
+//          Console.WriteLine("Busca de cliente");
+//          break;
         
-//         case "3":
-//         Console.WriteLine("Apagar cliente");
-//         break;
+//          case "3":
+//          Console.WriteLine("Apagar cliente");
+//          break;
         
-//         case "4":
-//         Console.WriteLine("Encerrar");
-//         exibirMenu = false;
-//         //Environment.Exit(0);        
-//         break;
+//          case "4":
+//          Console.WriteLine("Encerrar");
+//          exibirMenu = false;
+//          //Environment.Exit(0);        
+//          break;
 
-//         default:
-//             Console.WriteLine("Opção inválida");
-//             break;
-//     }
-// }
-// // Esta linha só será executada se declarada a variável boolean no case 4, de outra forma é encerrado na escolha do case 4
-// Console.WriteLine("O Programa foi finalizado");
+//          default:
+//              Console.WriteLine("Opção inválida");
+//              break;
+//      }
+//  }
+//  // Esta linha só será executada se declarada a variável boolean no case 4, de outra forma é encerrado na escolha do case 4
+//  Console.WriteLine("O Programa foi finalizado");
